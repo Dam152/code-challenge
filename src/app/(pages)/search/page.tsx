d26c0d25688e7c44256e754894cc86775d2fea0c
Damiano Ciucci <damjaguar98@icloud.com>
2025-09-04 15:20:35 +0200

    fix: import search template into page

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from 'next';
-import { PageHeader } from '@/components/molecules/PageHeader';
-import { SearchTemplate } from '@/components/tempate/SearchTemplate';
+import type { Metadata } from "next";
+import { PageHeader } from "@/components/molecules/PageHeader";
+import { SearchTemplate } from "@/components/tempate/SearchTemplate";
 
 export const metadata: Metadata = {
   title: `${process.env.NEXT_APP_NAME} | Search`,
-  description: 'search your favorite gifs',
+  description: "search your favorite gifs",
 };
 
 export default function page() {
   return (
     <div className="w-full grid gap-[19px]">
-      <PageHeader text={'Search'} labelLink={'back'} href={'/'} icon="prev" />
+      <PageHeader text={"Search"} labelLink={"back"} href={"/"} icon="prev" />
       <SearchTemplate />
     </div>
   );
