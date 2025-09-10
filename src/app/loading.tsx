e5ff4a580d5de04e2e5b5d1f1773e3697222f881
Damiano Ciucci <damjaguar98@icloud.com>
2025-09-10 12:46:33 +0200

    feat: loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+export default function loading() {
+  return (
+    <div className="!py-0 w-full h-screen flex items-center justify-center">
+      <PacmanLoader />
+    </div>
+  );
+}
