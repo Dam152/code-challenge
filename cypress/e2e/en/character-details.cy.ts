7a220c4dcd6c5cf4bf14b7a44c45fc9951d72814
Damiano Ciucci <damjaguar98@icloud.com>
2025-09-10 15:40:44 +0200

    fix:Character Details

--- cypress/e2e/en/character-details.cy.ts
+++ cypress/e2e/en/character-details.cy.ts
@@ -1,38 +1,38 @@
-describe('Character Details', () => {
-  it('should navigate to character detail from homepage', () => {
-    cy.visit('/en');
+describe("Character Details", () => {
+  it("should navigate to character detail from homepage", () => {
+    cy.visit("/en");
     cy.get('[data-testid="movie-card"]').first().click();
-    cy.url().should('include', '/character/');
-    cy.get('h1').should('exist');
+    cy.url().should("include", "/character/");
+    cy.get("h1").should("exist");
   });
 
-  it('should display character information', () => {
-    cy.visit('/en/character/1');
-    cy.get('h1').should('contain.text', 'Rick Sanchez');
-    cy.get('img').should('be.visible');
-    cy.contains('Human').should('be.visible');
-    cy.contains('Male').should('be.visible');
-    cy.contains('Alive').should('be.visible');
-    cy.contains('Origin').should('be.visible');
-    cy.contains('Location').should('be.visible');
+  it("should display character information", () => {
+    cy.visit("/en/character/1");
+    cy.get("h1").should("contain.text", "Rick Sanchez");
+    cy.get("img").should("be.visible");
+    cy.contains("Human").should("be.visible");
+    cy.contains("Male").should("be.visible");
+    cy.contains("Alive").should("be.visible");
+    cy.contains("Origin").should("be.visible");
+    cy.contains("Location").should("be.visible");
   });
 
-  it('should allow adding character to favorites from detail page', () => {
-    cy.visit('/en/character/1');
+  it("should allow adding character to favorites from detail page", () => {
+    cy.visit("/en/character/1");
     cy.get('[data-testid="favorite-button"]').click();
     cy.get('[data-testid="toast"]')
-      .should('be.visible')
-      .and('contain.text', 'added to favorites!');
+      .should("be.visible")
+      .and("contain.text", "added to favorites!");
   });
 
-  it('should handle invalid character ID', () => {
-    cy.visit('/en/character/99999', { failOnStatusCode: false });
-    cy.contains('404').should('be.visible');
+  it("should handle invalid character ID", () => {
+    cy.visit("/en/character/99999", { failOnStatusCode: false });
+    cy.contains("404").should("be.visible");
   });
 
-  it('should navigate back from character detail', () => {
-    cy.visit('/en/character/1');
-    cy.go('back');
-    cy.url().should('not.include', '/character/');
+  it("should navigate back from character detail", () => {
+    cy.visit("/en/character/1");
+    cy.go("back");
+    cy.url().should("not.include", "/character/");
   });
 });
