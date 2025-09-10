9c218e3ef511d594e501083abf18af0cc23dc1d6
Damiano Ciucci <damjaguar98@icloud.com>
2025-09-10 14:56:12 +0200

    fix: LanguageSwitcher

--- src/components/molecules/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/molecules/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import Cookies from 'js-cookie';
-import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
-import type { Locale } from '@/app/dictionaries';
+import Cookies from "js-cookie";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+import type { Locale } from "@/app/dictionaries";
 
 export function LanguageSwitcher() {
   const params = useParams();
   const pathname = usePathname();
   const currentLang = params.lang as Locale;
 
-  const nextLang: Locale = currentLang === 'it' ? 'en' : 'it';
+  const nextLang: Locale = currentLang === "it" ? "en" : "it";
   const newPath = pathname.replace(`/${currentLang}`, `/${nextLang}`);
 
   const handleLanguageChange = () => {
-    Cookies.set('locale', nextLang, {
+    Cookies.set("locale", nextLang, {
       expires: 365,
     });
   };
