4ca81272a2cd352781b41ff2cea26836ff49d374
Damiano Ciucci <damjaguar98@icloud.com>
2025-09-10 16:33:18 +0200

    feat:PageHeader

--- src/components/molecules/PageHeader/PageHeader.tsx
+++ src/components/molecules/PageHeader/PageHeader.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import { IoMdSearch } from 'react-icons/io';
-import { Text } from '@/components/atoms/Text';
-import { LanguageSwitcher } from '../LanguageSwitcher';
+import Link from "next/link";
+import { IoMdSearch } from "react-icons/io";
+import { Text } from "@/components/atoms/Text";
+import { LanguageSwitcher } from "../LanguageSwitcher";
 
 type PageHeaderProps = {
   text: string;
   labelLink: string;
   href: string;
-  icon?: 'prev' | 'next';
+  icon?: "prev" | "next";
 };
 
 export function PageHeader({
   text,
   labelLink,
   href,
-  icon = 'next',
+  icon = "next",
 }: PageHeaderProps) {
   return (
     <div className="w-full gap-[16px] flex max-[420px]:gap-[8px] flex-wrap justify-between items-center">
@@ -24,13 +24,13 @@ export function PageHeader({
       <div className="flex gap-4 items-center  max-[420px]:w-full">
         <LanguageSwitcher />
         <div className="max-[420px]:ml-auto flex items-center gap-4">
-          <Link href={'/search'}>
+          <Link href={"/search"}>
             <IoMdSearch className="w-[18px] h-[18px]" />
           </Link>
           <Link href={href} className="primary-link">
-            {icon === 'prev' && <span className="ml-2">←</span>}
+            {icon === "prev" && <span className="ml-2">←</span>}
             {labelLink}
-            {icon === 'next' && <span className="mr-2">→</span>}
+            {icon === "next" && <span className="mr-2">→</span>}
           </Link>
         </div>
       </div>
